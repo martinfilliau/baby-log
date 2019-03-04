9bc5ef08355290031c524692d2ce9cb4e8b6ab5f
Martin Filliau <martin@filliau.com>
2019-03-04 18:24:15 +0100

    elm-format

--- elm/src/main.elm
+++ elm/src/main.elm
@@ -1,106 +1,128 @@
+module Main exposing (Entries, Entry, Model, Msg(..), init, main, newEntry, update, view, viewEntries, viewEntry, viewKeyedEntry, viewNewEntry)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick)
+import Html.Events exposing (onClick, onInput)
 import Html.Keyed as Keyed
+
+
+
 -- import Date exposing (..)
-
-
 -- MAIN
 
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
 
 -- MODELS
 
-type alias Entry =
-  { key : String
-  , value : String
---  , date : Date
-  }
 
-type alias Entries = List Entry
+type alias Entry =
+    { key : String
+    , value : String
+
+    --  , date : Date
+    }
+
+
+type alias Entries =
+    List Entry
+
 
 type alias Model =
- {
-   entries: Entries
-   , newEntry: Entry
- }
+    { entries : Entries
+    , newEntry : Entry
+    }
+
 
 init : Model
-init = {
-  entries = [Entry "de" "mo"]
-  , newEntry = {key = "", value = ""}
-  }
+init =
+    { entries = [ Entry "de" "mo" ]
+    , newEntry = { key = "", value = "" }
+    }
+
 
 newEntry : Entry
-newEntry = {
-  key = ""
-  , value = ""
-  }
+newEntry =
+    { key = ""
+    , value = ""
+    }
+
+
 
 -- UPDATE
 
+
 type Msg
-  = Add
-  | UpdateNewKey String
-  | UpdateNewValue String
+    = Add
+    | UpdateNewKey String
+    | UpdateNewValue String
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Add ->
-      {
-        entries = model.entries ++ [model.newEntry],
-        newEntry = {key = "", value = ""}
-      }
-    UpdateNewKey str ->
-      {
-        newEntry = {
-          key = str
-          , value = model.newEntry.value
-        },
-        entries = model.entries
-      }
-    UpdateNewValue str ->
-      {
-        newEntry = {
-          key = model.newEntry.key
-          , value = str
-        },
-        entries = model.entries
-      }
+    case msg of
+        Add ->
+            { entries = model.entries ++ [ model.newEntry ]
+            , newEntry = { key = "", value = "" }
+            }
+
+        UpdateNewKey str ->
+            { newEntry =
+                { key = str
+                , value = model.newEntry.value
+                }
+            , entries = model.entries
+            }
+
+        UpdateNewValue str ->
+            { newEntry =
+                { key = model.newEntry.key
+                , value = str
+                }
+            , entries = model.entries
+            }
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ viewNewEntry model.newEntry
-    , viewEntries model.entries ]
+    div []
+        [ viewNewEntry model.newEntry
+        , viewEntries model.entries
+        ]
+
 
 viewNewEntry : Entry -> Html Msg
 viewNewEntry entry =
-  div []
-    [ input [ type_ "text", placeholder "Type", value entry.key, onInput UpdateNewKey ] []
-    , input [ type_ "text", placeholder "Valeur", value entry.value, onInput UpdateNewValue ] []  
-    , button [ onClick (Add) ] [ text "+" ]
-  ]
+    div []
+        [ input [ type_ "text", placeholder "Type", value entry.key, onInput UpdateNewKey ] []
+        , input [ type_ "text", placeholder "Valeur", value entry.value, onInput UpdateNewValue ] []
+        , button [ onClick Add ] [ text "+" ]
+        ]
+
 
 viewEntries : Entries -> Html Msg
 viewEntries entries =
     section []
-        [ 
-            Keyed.node "ul" [] (List.map viewKeyedEntry entries)
+        [ Keyed.node "ul" [] (List.map viewKeyedEntry entries)
         ]
 
-viewKeyedEntry : Entry -> (String, Html Msg)
+
+viewKeyedEntry : Entry -> ( String, Html Msg )
 viewKeyedEntry entry =
     ( entry.key, viewEntry entry )
 
+
 viewEntry : Entry -> Html Msg
-viewEntry entry = 
-  div []
-      [ div [] [ text entry.key ]
-      , div [] [ text entry.value ]
-      ]
+viewEntry entry =
+    div []
+        [ div [] [ text entry.key ]
+        , div [] [ text entry.value ]
+        ]
